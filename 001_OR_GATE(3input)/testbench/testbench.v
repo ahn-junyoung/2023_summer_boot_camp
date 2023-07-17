ac9f200a568beb767812036f8c025d37a69c53bf
ahn-junyoung <wnsdud1338@naver.com>
2023-07-17 15:24:20 +0900

    or_gate_3input

---
+++ 001_OR_GATE(3input)/testbench/testbench.v
@@ -0,0 +1,31 @@
+module testbench;
+reg a;
+reg b;
+reg c;
+wire out;
+
+    or_gate uut_or_gate (
+    .a(a),
+    .b(b),
+    .c(c),
+    .out(out)
+);
+
+initial begin
+    a = 0; b = 0; c = 0;
+    #5;
+    a = 0; b = 0; c = 1;
+    #5;
+    a = 0; b = 1; c = 0;
+    #5;
+    a = 1; b = 0; c = 0;
+    #5;
+    a = 1; b = 1; c = 0;
+    #5;
+    a = 1; b = 1; c = 1;
+    #5;
+
+    $stop;
+
+end
+endmodule
